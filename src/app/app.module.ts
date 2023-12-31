a44db50abd4dadc6c79da1584c2cce2b1653fa94
hamza-elgarai <hamzaelgarai10@gmail.com>
2024-01-01 00:56:15 +0100

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EdbHeaderComponent } from './edb-header/edb-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EdbHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/edb-header/edb-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edb-header',
+  templateUrl: './edb-header.component.html',
+  styleUrls: ['./edb-header.component.css']
+})
+export class EdbHeaderComponent {
+
+}
